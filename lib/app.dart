c930021eacbc6c6d77c34a40104d48e2ce82af50
imon <imonsaha153@gmail.com>
2024-07-16 11:56:34 +0600

    Initial desing of sing_in done

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
+
 import 'package:flutter/material.dart';
 import 'package:maneger/ui/sceens/splash_sceen.dart';
+import 'package:maneger/ui/utility/app_colors.dart';
 
 class TaskManeger extends StatelessWidget {
   const TaskManeger({super.key});
@@ -8,6 +10,48 @@ class TaskManeger extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SplashSceen(),
+      theme: lightThemeData(),
+    );
+  }
+
+  ThemeData lightThemeData() {
+    return ThemeData(
+      inputDecorationTheme:   InputDecorationTheme(
+        fillColor: Colors.white,
+        filled: true,
+        hintStyle: TextStyle(
+           color:  Colors.grey.shade300
+        ),
+        border: OutlineInputBorder(
+            borderSide: BorderSide.none
+        )
+    ),
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.black
+        ),
+        // titleMedium: TextStyle(
+        //   fontWeight: FontWeight.w400,
+        //   fontSize: 16,
+        //   color: Colors.green
+        // )
+      ),
+
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.themeColor,
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          foregroundColor: AppColors.white,
+
+          fixedSize: Size.fromWidth(double.maxFinite),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8)
+          )
+
+        )
+      )
     );
   }
 }
--- lib/ui/sceens/sing_in_sceen.dart
+++ lib/ui/sceens/sing_in_sceen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart.';
+import 'package:maneger/ui/utility/app_colors.dart';
+import 'package:maneger/ui/weidgets/background_weidgets.dart';
 
 class SingInSceen extends StatefulWidget {
   const SingInSceen({super.key});
@@ -8,10 +11,81 @@ class SingInSceen extends StatefulWidget {
 }
 
 class _SingInSceenState extends State<SingInSceen> {
+  final TextEditingController _emailTEcontroller = TextEditingController();
+  final TextEditingController _passwordTEcontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('imon'),
+      body: BackgroundWeidgets(child:
+      SafeArea(child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 80,),
+              Text("Get Started With",style: Theme.of(context).textTheme.titleLarge,),
+              SizedBox(height: 10,),
+              TextFormField(
+                controller: _emailTEcontroller,
+                decoration: InputDecoration(
+                  hintText: 'Email'
+                ),
+        
+              ),
+              const SizedBox(height: 8,),
+              TextFormField(
+                controller: _passwordTEcontroller,
+                decoration: InputDecoration(
+                    hintText: 'Password'
+                ),
+        
+              ),
+              SizedBox(height: 15,),
+              ElevatedButton(onPressed: (){},
+                  child: Icon(Icons.arrow_circle_right_outlined)
+              ),
+            const SizedBox(height: 30,),
+              Center(
+                child: Column(
+                  children: [
+                    TextButton(onPressed: (){},
+                        child: Text('Forget Password')),
+                    RichText(text:TextSpan(
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 0.5
+                      ),
+                      text: "Don't have an account?",
+                      children: [
+                        TextSpan(
+                          style: TextStyle(
+                            color: AppColors.themeColor,
+
+                          ),
+                          text: "Sing Up",
+                          recognizer: TapGestureRecognizer()..onTap=(){
+                            print('hello');
+                          }
+                        )
+                      ]
+                    )),
+                  ],
+                ),
+              )
+
+            ],
+          ),
+        ),
+      ))),
     );
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    _passwordTEcontroller.dispose();
+    _emailTEcontroller.dispose();
   }
 }
