fb10264fe5ea795394dc00c49f14031ef8d7bb1a
imon <imonsaha153@gmail.com>
2024-07-28 00:07:06 +0600

    In singUp page form validation Added and password on/of added

--- lib/ui/sceens/auth/sing_up_sceen.dart
+++ lib/ui/sceens/auth/sing_up_sceen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart.';
 import 'package:maneger/ui/utility/app_colors.dart';
+import 'package:maneger/ui/utility/app_constants.dart';
 import 'package:maneger/ui/weidgets/background_weidgets.dart';
 
 class SingUpSceen extends StatefulWidget {
@@ -16,6 +17,8 @@ class _SingInSceenState extends State<SingUpSceen> {
   final TextEditingController _firstNameTEcontroller = TextEditingController();
   final TextEditingController _lastNameTEcontroller = TextEditingController();
   final TextEditingController _mobileTEcontroller = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _showPassword  = false;
 
   @override
   Widget build(BuildContext context) {
@@ -24,62 +27,117 @@ class _SingInSceenState extends State<SingUpSceen> {
       SafeArea(child: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 80,),
-              Text("Join With Us",style: Theme.of(context).textTheme.titleLarge,),
-              SizedBox(height: 10,),
-              TextFormField(
-                controller: _emailTEcontroller,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    hintText: 'Email'
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 80,),
+                Text("Join With Us",style: Theme.of(context).textTheme.titleLarge,),
+                SizedBox(height: 10,),
+                TextFormField(
+                  controller: _emailTEcontroller,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      hintText: 'Email'
+                  ),
+                  validator: (String? value){
+                    if(value?.trim().isEmpty ?? true){
+                      return "Enter your email";
+                    }
+                    return null;
+                  },
+
+                ),
+                const SizedBox(height: 8,),
+                TextFormField(
+                  controller: _firstNameTEcontroller,
+                  decoration: InputDecoration(
+                      hintText: 'FirstName'
+                  ),
+                  validator: (String? value){
+                    if(value?.trim().isEmpty ?? true){
+                      return "Enter your firstName";
+                    }
+                    return null;
+                  },
+
+                ),
+                const SizedBox(height: 8,),
+                TextFormField(
+                  controller: _lastNameTEcontroller,
+                  decoration: InputDecoration(
+                      hintText: 'LastName'
+                  ),
+                  validator: (String? value){
+                    if(value?.trim().isEmpty ?? true){
+                      return "Enter your lastName";
+                    }
+                    return null;
+                  },
+
+                ),
+                const SizedBox(height: 8,),
+                TextFormField(
+                  controller: _mobileTEcontroller,
+                  keyboardType: TextInputType.numberWithOptions(),
+                  decoration: InputDecoration(
+                      hintText: 'Mobile'
+                  ),
+                  validator: (String? value){
+                    if(value?.trim().isEmpty ?? true){
+                      return "Enter your mobileNumber";
+                    }
+                    return null;
+                  },
+
+                ),
+                const SizedBox(height: 8,),
+                TextFormField(
+                  obscureText: _showPassword==false,
+                  controller: _passwordTEcontroller,
+
+                  decoration: InputDecoration(
+                      hintText: 'Password',
+                    suffixIcon: IconButton(onPressed:
+                        (){
+                      _showPassword = !_showPassword;
+                      if(mounted){
+                        setState(() {
+
+                        });
+                      }
+
+                        },
+                        icon: Icon( _showPassword ? Icons.visibility_off  : Icons.remove_red_eye))
+                  ),
+                  validator: (String? value) {
+                    if (value?.trim().isEmpty ?? true) {
+                      return 'Enter your email address';
+                    }
+                    // if (AppConstants.emailRegExp.hasMatch(value!) ==
+                    //     false) {
+                    //   return 'Enter a valid email address';
+                    // }
+                    return null;
+                  },
                 ),
 
-              ),
-              const SizedBox(height: 8,),
-              TextFormField(
-                controller: _firstNameTEcontroller,
-                decoration: InputDecoration(
-                    hintText: 'FirstName'
+
+                SizedBox(height: 15,),
+                ElevatedButton(onPressed: (){
+                  if(_formKey.currentState!.validate()){
+
+                  }
+                },
+                    child: Icon(Icons.arrow_circle_right_outlined)
                 ),
-
-              ),
-              const SizedBox(height: 8,),
-              TextFormField(
-                controller: _lastNameTEcontroller,
-                decoration: InputDecoration(
-                    hintText: 'LastName'
-                ),
-
-              ),
-              const SizedBox(height: 8,),
-              TextFormField(
-                controller: _mobileTEcontroller,
-                keyboardType: TextInputType.numberWithOptions(),
-                decoration: InputDecoration(
-                    hintText: 'Mobile'
-                ),
-
-              ),
-              const SizedBox(height: 8,),
-              TextFormField(
-                controller: _passwordTEcontroller,
-                decoration: InputDecoration(
-                    hintText: 'Password'
-                ),
-
-              ),
-              SizedBox(height: 15,),
-              ElevatedButton(onPressed: (){},
-                  child: Icon(Icons.arrow_circle_right_outlined)
-              ),
-              const SizedBox(height: 30,),
-              _buildBackToSingInSceen(),
+                const SizedBox(height: 30,),
+                _buildBackToSingInSceen(),
 
 
-            ],
+              ],
+            ),
           ),
         ),
       ))),
